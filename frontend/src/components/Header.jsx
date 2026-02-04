c8eefc85cfca1f97ee807f32f0e93edeff1a9984
BASSprogrammer <bariassalvo@gmail.com>
2026-02-04 00:47:42 -0300

    Update css style mobile

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -7,22 +7,22 @@ export default function Header({dataCart, deleteCart, emptyCart}){
     const nombretienda = "GuitarStore"
     return(
         <header className="sticky-menu">
-            <div className="header-container flex justify-between flex-wrap align-center">
-                <div className="header-logo left flex align-center">
+            <div className="header-container align-center">
+                <div className="header-logo left align-center">
                     <img src={require("../assets/images/header/guitar.png")} alt="Logo" width={'50px'} height={'50px'}></img>
                     <h1>{nombretienda}</h1> 
                 </div>
                 <div>
                     <Menu/>
                 </div>
-                <div className="darkmode-shop right flex align-center gap-2 position-absolute top-0 right-0">
-                    <Switch/>
-                    <ShoppingCart
-                        dataCart = {dataCart}
-                        deleteCart={deleteCart} 
-                        emptyCart = {emptyCart}
-                    />
-                </div>
+            </div>
+            <div className="darkmode-shop right flex align-center gap-2 position-absolute top-0 right-0">
+                <Switch/>
+                <ShoppingCart
+                    dataCart = {dataCart}
+                    deleteCart={deleteCart} 
+                    emptyCart = {emptyCart}
+                />
             </div>
         </header>
     )
